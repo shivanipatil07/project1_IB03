4f6c1660b3c70328f3d53e8dd039b019dc0488da
madhuri_02 <madhuri0219patil@gmail.com>
2023-01-29 22:27:31 +0530

    chairperson page

--- src/react-assign-1/Navbar/Chair-person/Chairperson.js
+++ src/react-assign-1/Navbar/Chair-person/Chairperson.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import './Chairperson.css'
+import {FaLinkedinIn, FaInstagram,FaTwitter, FaWhatsapp} from "react-icons/fa";
+import chair from'../assets/chair-person-img.png'
+
 
 export default function Chairperson() {
   return (
@@ -7,26 +10,34 @@ export default function Chairperson() {
       <div className='chair-main'>
 
         <div className='chair-img'>
-          <h1>Chair-Person-Image</h1>
-          <div className='social-icon'>
-            <h3>Social-Icon</h3>
-          </div>
-        </div>
+        <img src={chair} alt="chair"/>
+         
+           <div className='social-icon'>
+            <div className='container'>
+                <h3><FaInstagram/></h3>
+                <div className='dropdown'>
+                  <a href=''><FaLinkedinIn/></a>
+                  <a href=''><FaTwitter/></a>
+                  <a href=''><FaWhatsapp/></a>
+                </div>
+            </div>
+           </div>
+          
+      </div>
 
         <div className='chair-person'>
           <h1>Chair Person</h1>
           <h2>XYZ Person</h2>
           <p>It is a long established fact that a reader will be
             distracted by the readable content of a page
-            when looking at its layout. The point oflike
+            when looking at itslayout. The point oflike
             readable English. Many desktop publishing
             search for 'lorem ipsum' will uncover many web
             sites still in their infancy. Various versions have
             evolved over the years, sometimes by accident,
-            sometimes on purpose (infancy. Various
+            sometimes on purpose infancy. Various
             versions have evolved over the years,
-            sometimes by accident, sometimes on purpose
-            (injected humour and the like).</p>
+            sometimes by accident,sometimes on purpose.</p>
         </div>
       </div>
     </div>
