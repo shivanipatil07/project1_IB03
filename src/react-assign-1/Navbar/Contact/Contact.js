95707515569b3498319e385e7dc15fa7fe5891bd
shivanipatil07 <soveshivani@gmail.com>
2023-01-30 08:15:10 +0530

    image added in assets folder

--- src/react-assign-1/Navbar/Contact/Contact.js
+++ src/react-assign-1/Navbar/Contact/Contact.js
@@ -1,14 +1,36 @@
 import React from 'react'
 import './Contact.css'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 export default function Contact() {
     return (
         <div className='contact-main'>
 
             <div className='contact-sub2'>
-                <h1>Contact-Form</h1>
+            <Form className='form'>
+      <Form.Group className="mb-4" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+ 
             </div>
             <div className='contact-sub1'>
-                <h1>Full-Size-Image</h1>
+                
             </div>
 
 
