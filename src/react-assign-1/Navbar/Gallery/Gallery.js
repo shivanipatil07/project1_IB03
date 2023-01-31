3c122daa01b0ffd9e9c064c7625353cf50a3499c
ganeshkalbaile <ganeshkk1994@gmail.com>
2023-01-31 10:45:06 +0530

    gallary page created

--- src/react-assign-1/Navbar/Gallery/Gallery.js
+++ src/react-assign-1/Navbar/Gallery/Gallery.js
@@ -1,35 +1,40 @@
 import React from 'react'
 import './Gallery.css'
+import galimg1 from '../assets/gallary-1.jpg'
+import galimg2 from '../assets/gallary-2.jpg'
+import galimg3 from '../assets/gallary-3.jpg'
+import galimg4 from '../assets/gallary-4.jpg'
+import galimg5 from '../assets/gallary-5.jpg'
 
 export default function Gallery() {
   return (
     <div className='Gallery-main'>
         <h1>Gallery</h1>
 
-        <div className='Gland-page'>
+        <div className='Gland-page' data-aos="fade-in">
 
             <div className='G-1'>
               <div className='G-img-1'>
-                <h2>Image</h2>
+              <img src={galimg1} alt="img"/>
               </div>
               <div className='G-img-2'>
-                <h2>Image</h2>
+              <img src={galimg2} alt="img"/>
               </div>
             </div>
 
             <div className='G-2'>
               <div className='G-img-2-2'>
-               <h2>Image</h2>
+              <img src={galimg3} alt="img"/>
               </div>
             </div>
 
 
             <div className='G-3'>
               <div className='G-img-3-1'>
-              <h2>Image</h2>
+              <img src={galimg4} alt="img"/>
               </div>
               <div className='G-img-3-2'>
-              <h2>Image</h2>
+              <img src={galimg5} alt="img"/>
                 </div>
             </div>
         </div>
