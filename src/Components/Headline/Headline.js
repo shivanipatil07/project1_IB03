432f891af369618dcd30c665ca5b2310ad51c759
snehal-bhoir12 <snehalbhoir1999@gmail.com>
2023-01-31 15:12:22 +0530

    HeadImg

--- src/Components/Headline/Headline.js
+++ src/Components/Headline/Headline.js
@@ -1,25 +1,25 @@
 import React from 'react'
 import './Headline.css'
 import { AiOutlineArrowRight} from "react-icons/ai";
-import Head1 from '../../Assets/Himg-1.jpg'
-import Head2 from '../../Assets/Himg1.jpeg'
-import Head3 from '../../Assets/Himh-2.jpg'
+import Head1 from '../../Assets/H1.jpg'
+import Head2 from '../../Assets/H1.jpg'
+import Head3 from '../../Assets/H2.jpeg'
 export default function Headline() {
   return (
     <div className='head'>
 
       <div className='div-1'>
-        <div className='img-1'>
+        <div className='img-1' data-aos="fade-right">
         <img src={Head1} alt="img"  />
           <h1></h1>
         </div>
 
-        <div className='img-3'>
-        <img src={Head2} alt="img2"  width={250}  />
+        <div className='img-3'data-aos="fade-up-left">
+        <img src={Head2} alt="img2" />
           <h1></h1>
         </div>
 
-        <div className='img-2'>
+        <div className='img-2' data-aos="flip-right" data-aos-duration="1000">
         <img src={Head3} alt="img2"   />
           <h1></h1>
         </div>
