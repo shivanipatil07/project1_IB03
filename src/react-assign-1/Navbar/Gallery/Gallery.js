35ddf153bc11e5e1cfe1118487e297f552b53c48
ganeshkalbaile <ganeshkk1994@gmail.com>
2023-01-31 11:22:25 +0530

    gallary page created

--- src/react-assign-1/Navbar/Gallery/Gallery.js
+++ src/react-assign-1/Navbar/Gallery/Gallery.js
@@ -11,18 +11,18 @@ export default function Gallery() {
     <div className='Gallery-main'>
         <h1>Gallery</h1>
 
-        <div className='Gland-page' data-aos="fade-in">
+        <div className='Gland-page'>
 
             <div className='G-1'>
-              <div className='G-img-1'>
+              <div className='G-img-1' data-aos="fade-left" data-aos-duration="1000">
               <img src={galimg1} alt="img"/>
               </div>
-              <div className='G-img-2'>
+              <div className='G-img-2' data-aos="fade-right" data-aos-offset="200" data-aos-duration="1000">
               <img src={galimg2} alt="img"/>
               </div>
             </div>
 
-            <div className='G-2'>
+            <div className='G-2' data-aos="zoom-in-up"  data-aos-duration="1000">
               <div className='G-img-2-2'>
               <img src={galimg3} alt="img"/>
               </div>
@@ -30,10 +30,10 @@ export default function Gallery() {
 
 
             <div className='G-3'>
-              <div className='G-img-3-1'>
+              <div className='G-img-3-1' data-aos="fade-right" data-aos-duration="1000">
               <img src={galimg4} alt="img"/>
               </div>
-              <div className='G-img-3-2'>
+              <div className='G-img-3-2' data-aos="fade-left" data-aos-duration="1000">
               <img src={galimg5} alt="img"/>
                 </div>
             </div>
